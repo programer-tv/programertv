1497680bf80e4e5f89fc15b23ff4e5d17c453735
moeabdol <admin.r99@gmail.com>
2015-12-15 04:12:42 +0300

    user can't create course with invalid attributes

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -1,4 +1,5 @@
 require 'rails_helper'
+require "pry"
 
 describe CoursesController, type: :controller do
   context "GET" do
@@ -67,6 +68,23 @@ describe CoursesController, type: :controller do
         it "redirects to show page" do
           course = assigns(:course)
           expect(response).to redirect_to(course)
+        end
+      end
+
+      context "with invalid attributes" do
+        before(:example) { post :create,
+                           course: attributes_for(:course, en_title: nil) }
+
+        it "returns http success" do
+          expect(response).to have_http_status(:success)
+        end
+
+        it "assigns @course" do
+          expect(assigns(:course)).to be_present
+        end
+
+        it "renders new template" do
+          expect(response).to render_template(:new)
         end
       end
     end
--- spec/features/course_crud_spec.rb
+++ spec/features/course_crud_spec.rb
@@ -2,19 +2,32 @@ require "rails_helper"
 require "pry"
 
 describe "user adds course" do
+  let(:course) { build(:course) }
+  before(:example) { visit new_course_path }
+
   context "with valid attributes" do
     it "adds course successfully" do
-      course = build(:course)
-      visit new_course_path
-      fill_in "course_ar_title",             with: course.ar_title
-      fill_in "course_en_title",             with: course.en_title
-      fill_in "course_description",          with: course.description
-      select course.video_host.to_s,  from: "course_video_host"
-      fill_in "course_video_id",      with: course.video_id
+      fill_in "course_ar_title"     , with: course.ar_title
+      fill_in "course_en_title"     , with: course.en_title
+      fill_in "course_description"  , with: course.description
+      select course.video_host.to_s , from: "course_video_host"
+      fill_in "course_video_id"     , with: course.video_id
       check "course_premium"
       check "course_active"
       click_on "Create Course"
       expect(page).to have_text(course.en_title)
+    end
+  end
+
+  context "with invalid attributes" do
+    it "fails and renders new template" do
+      fill_in "course_ar_title"     , with: nil
+      fill_in "course_en_title"     , with: nil
+      fill_in "course_description"  , with: nil
+      select course.video_host.to_s , from: "course_video_host"
+      fill_in "course_video_id"     , with: nil
+      click_on "Create Course"
+      expect(page).to have_text("can't be blank")
     end
   end
 end
