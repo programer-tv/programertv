04a1da9a2f6d1e7e563cce903661ee624f359440
moeabdol <admin.r99@gmail.com>
2015-12-16 07:06:01 +0300

    test course controller for PATCH #update

--- spec/controllers/courses_controller_spec.rb
+++ spec/controllers/courses_controller_spec.rb
@@ -102,4 +102,42 @@ describe CoursesController, type: :controller do
       end
     end
   end
+
+  describe "PATCH #update" do
+    let(:course) { create(:course) }
+
+    context "with valid attributes" do
+      before(:example) { patch :update, id: course,
+                         course: attributes_for(:course) }
+
+      it "returns http redirect" do
+        expect(response).to have_http_status(:redirect)
+      end
+
+      it "assigns @course" do
+        expect(assigns(:course)).to be_present
+      end
+
+      it "redirects to show page" do
+        expect(response).to redirect_to(course)
+      end
+    end
+
+    context "with invalid attributes" do
+      before(:example) { patch :update, id: course,
+                         course: attributes_for(:course, ar_title: nil) }
+
+      it "returns http success" do
+        expect(response).to have_http_status(:success)
+      end
+
+      it "assigns @course" do
+        expect(assigns(:course)).to be_present
+      end
+
+      it "renders edit page" do
+        expect(response).to render_template(:edit)
+      end
+    end
+  end
 end
