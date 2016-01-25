0d1cd7bf71cc648e983829cf994058af652877f1
moeabdol <admin.r99@gmail.com>
2016-01-26 00:18:40 +0300

    Render Youtube, Wistia, and Viemo iframes
    * Implement video iframe helpers in application helper
    * Render iframes in course and episode show views
    * Implement specs to test iframe helpers for all video hosts

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -17,10 +17,41 @@ module ApplicationHelper
     render_haml("<div id='disqus_thread'></div>")
 	end
 
+  def video_iframe(object)
+    if object.video_host == "YouTube"
+      render_haml(youtube + object.video_id + video_settings)
+    elsif object.video_host == "Wistia"
+      render_haml(wistia + object.video_id + video_settings)
+    elsif object.video_host == "Vimeo"
+      render_haml(vimeo + object.video_id + video_settings)
+    end
+  end
+
 	private
 
   def render_haml(code)
     engine = Haml::Engine.new(code)
     engine.render
+  end
+
+  def youtube
+    "%iframe{ src: 'https://www.youtube.com/embed/"
+  end
+
+  def wistia
+    "%iframe{ src: 'https://fast.wistia.net/embed/iframe/"
+  end
+
+  def vimeo
+    "%iframe{ src: 'https://player.vimeo.com/video/"
+  end
+
+  def video_settings
+    "', webkitallowfullscreen: true, " +
+    "oallowfullscreen: true, " +
+    "mozallowfullscreen: true, " +
+    "msallowfullscreen: true, " +
+    "allowfullscreen: true, " +
+    "class: 'center-block' }"
   end
 end
--- spec/helpers/application_helper_spec.rb
+++ spec/helpers/application_helper_spec.rb
@@ -20,4 +20,66 @@ describe ApplicationHelper, type: :helper do
 			expect(disqus_comments).to eq("<div id='disqus_thread'></div>\n")
     end
   end
+
+  context "video iframes" do
+    it "displays YouTube iframe correctly in course show view" do
+			course = create(:course, video_host: "YouTube", video_id: "12345")
+        expect(video_iframe(course)).to \
+          eq("<iframe allowfullscreen class='center-block' " +
+            "mozallowfullscreen " +
+            "msallowfullscreen oallowfullscreen " +
+            "src='https://www.youtube.com/embed/12345' " +
+            "webkitallowfullscreen></iframe>\n")
+    end
+
+    it "displays YouTube iframe correctly in episode show view" do
+			episode = create(:episode, video_host: "YouTube", video_id: "12345")
+        expect(video_iframe(episode)).to \
+          eq("<iframe allowfullscreen class='center-block' " +
+            "mozallowfullscreen " +
+            "msallowfullscreen oallowfullscreen " +
+            "src='https://www.youtube.com/embed/12345' " +
+            "webkitallowfullscreen></iframe>\n")
+    end
+
+    it "displays Wistia iframe correctly in course show view" do
+			course = create(:course, video_host: "Wistia", video_id: "12345")
+        expect(video_iframe(course)).to \
+          eq("<iframe allowfullscreen class='center-block' " +
+            "mozallowfullscreen " +
+            "msallowfullscreen oallowfullscreen " +
+            "src='https://fast.wistia.net/embed/iframe/12345' " +
+            "webkitallowfullscreen></iframe>\n")
+    end
+
+    it "displays Wistia iframe correctly in episode show view" do
+			episode = create(:episode, video_host: "Wistia", video_id: "12345")
+        expect(video_iframe(episode)).to \
+          eq("<iframe allowfullscreen class='center-block' " +
+            "mozallowfullscreen " +
+            "msallowfullscreen oallowfullscreen " +
+            "src='https://fast.wistia.net/embed/iframe/12345' " +
+            "webkitallowfullscreen></iframe>\n")
+    end
+
+    it "displays Vimeo iframe correctly in course show view" do
+			course = create(:course, video_host: "Vimeo", video_id: "12345")
+        expect(video_iframe(course)).to \
+          eq("<iframe allowfullscreen class='center-block' " +
+            "mozallowfullscreen " +
+            "msallowfullscreen oallowfullscreen " +
+            "src='https://player.vimeo.com/video/12345' " +
+            "webkitallowfullscreen></iframe>\n")
+    end
+
+    it "displays Vimeo iframe correctly in episode show view" do
+			episode = create(:episode, video_host: "Vimeo", video_id: "12345")
+        expect(video_iframe(episode)).to \
+          eq("<iframe allowfullscreen class='center-block' " +
+            "mozallowfullscreen " +
+            "msallowfullscreen oallowfullscreen " +
+            "src='https://player.vimeo.com/video/12345' " +
+            "webkitallowfullscreen></iframe>\n")
+    end
+  end
 end
