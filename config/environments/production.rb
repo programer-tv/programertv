6f8e359119fe06db80354ce71df84dd117f28a50
moeabdol <admin.r99@gmail.com>
2016-02-01 02:42:30 +0300

    Modify production.rb mailer configs

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -79,9 +79,8 @@ Rails.application.configure do
 
   # Configure Action Mailer
   config.action_mailer.perform_deliveries = true
-  config.action_mailer.raise_delivery_errors = false
+  config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'https://programer.tv' }
-  config.action_mailer.asset_host = 'https://programer.tv'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
