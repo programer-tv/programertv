c096f8f2ddb28cfecbe3881f75c5d0afb32f442a
moeabdol <admin.r99@gmail.com>
2016-02-01 02:58:45 +0300

    Configure assets host to production.rb

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -81,6 +81,7 @@ Rails.application.configure do
   config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { host: 'https://programer.tv' }
+  config.action_mailer.asset_host = 'https://programer.tv'
   config.action_mailer.delivery_method = :smtp
   config.action_mailer.smtp_settings = {
     address: 'smtp.sendgrid.net',
