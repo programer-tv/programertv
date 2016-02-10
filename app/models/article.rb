c4d8a901d5fd536b73e094aa17123cdf41231198
moeabdol <admin.r99@gmail.com>
2016-02-10 15:34:59 +0300

    Add slug to articles using friendly_id

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,6 +1,13 @@
 class Article < ActiveRecord::Base
+  extend FriendlyId
+  friendly_id :en_title, use: :slugged
+
   validates :ar_title,  presence: true,  uniqueness: true
   validates :en_title,  presence: true,  uniqueness: true
   validates :author,    presence: true
   validates :content,   presence: true
+
+  def should_generate_new_friendly_id?
+    en_title_changed?
+  end
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160210080552) do
+ActiveRecord::Schema.define(version: 20160210123023) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -24,7 +24,10 @@ ActiveRecord::Schema.define(version: 20160210080552) do
     t.boolean  "active"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.string   "slug"
   end
+
+  add_index "articles", ["slug"], name: "index_articles_on_slug", unique: true, using: :btree
 
   create_table "ckeditor_assets", force: :cascade do |t|
     t.string   "data_file_name",               null: false
